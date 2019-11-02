9c46f496d1df1b33fb9c3746071063a2f4dc26ab
aka-jain <akashjain993@gmail.com>
2019-11-02 19:06:55 +0530

    Updates

---
+++ precache-manifest.bb3ac473fa4dc9371e83bdaa84f94c2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02904ce91980cf915a44be084adf5fd7",
+    "url": "/campaign/index.html"
+  },
+  {
+    "revision": "25a3b383d3f8484bdc3b",
+    "url": "/campaign/static/css/2.ff9ea1f9.chunk.css"
+  },
+  {
+    "revision": "1a840dfeefe8612b41c7",
+    "url": "/campaign/static/css/main.abfebd43.chunk.css"
+  },
+  {
+    "revision": "25a3b383d3f8484bdc3b",
+    "url": "/campaign/static/js/2.b360ac40.chunk.js"
+  },
+  {
+    "revision": "1a840dfeefe8612b41c7",
+    "url": "/campaign/static/js/main.56a8b150.chunk.js"
+  },
+  {
+    "revision": "19b1578ac96920148774",
+    "url": "/campaign/static/js/runtime-main.f81d5a4d.js"
+  }
+]);
